138e4e559fabffc86f95cb7c6b5ac1f6713443ab
bhatt <bhatt.meena@yahoo.com>
2024-09-20 14:31:38 -0400

    name

--- fruitsales.py
+++ fruitsales.py
@@ -1,9 +1,10 @@
 # add your code here
 # DataFrame 
 import pandas as pd
+
 fruit_sales = pd.DataFrame({
     'Apples': [35, 41], 
     'Bananas': [21, 34]}, 
     index=['2017 Sales','2018 Sales'])
 fruit_sales
-fruit_sales.to_csv('fruit_sales.csv')
+fruit_sales.to_csv('fruit.csv')
